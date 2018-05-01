212e634939ddf1a49bf36ce46ef13553080a28b7
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-01 00:09:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,24 +1,4 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray() {
-  var myArray = [1];
-  var newArray = ['foo', ...myArray];
-    return newArray;
-}
 
-function destructivelyAddElementToBeginningOfArray() {
-  var thisArray = [1];
-  return thisArray.unshift('foo');
-}
 
-function addElementToEndOfArray() {
-  var myArray = [1]
-  var newArray = [...myArray, 'foo']
-    return newArray
-}
-
-function destructivelyAddElementToEndOfArray() {
-  var myArray = [1]
-  myArray.push('foo')
-    return myArray
-}
